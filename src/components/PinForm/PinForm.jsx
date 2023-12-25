408fd591bfbea30bcdaf054a05c2f96445da6cdb
anuj7862 <anujtiwarimnnit@gmail.com>
2023-12-26 00:45:37 +0530

    added code for tagged topics in Pin Form and other updates

--- src/components/PinForm/PinForm.jsx
+++ src/components/PinForm/PinForm.jsx
@@ -5,19 +5,39 @@ import { useState } from 'react';
 import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 import { FaCircleChevronRight } from "react-icons/fa6";
 import { MdEdit } from "react-icons/md";
+import { IoCloseSharp } from "react-icons/io5";
 
 import SelectBoardBox from '../SelectBoardBox/SelectBoardBox';
 
 import './PinForm.css';
 
 function PinForm() {
+
+    const boards = useSignal([
+        { id: 'board1', name: 'Board 1', isLocked: true, image: 'https://i.pinimg.com/75x75/8b/0b/e3/8b0be3749ca05848ce6b7d3fe5d3983f.jpg' },
+        { id: 'board2', name: 'Board 2', isLocked: false, image: 'https://i.pinimg.com/75x75/92/29/64/922964c806076e8975d494af90dcdffe.jpg' },
+        { id: 'board3', name: 'Board 3', isLocked: true, image: 'https://i.pinimg.com/75x75/8b/0b/e3/8b0be3749ca05848ce6b7d3fe5d3983f.jpg' },
+        { id: 'board4', name: 'Board 4', isLocked: false, image: 'https://i.pinimg.com/75x75/92/29/64/922964c806076e8975d494af90dcdffe.jpg' },
+    ]);
+
+    const topics = useSignal([
+        {id: 'topic1', name: 'topic1'},
+        {id: 'topic2', name: 'topic2'},
+        {id: 'topic3', name: 'topic3'},
+        {id: 'topic4', name: 'topic4'},
+        {id: 'topic5', name: 'topic5'},
+        {id: 'topic6', name: 'topic6'},
+    ]);
+
     const title= useSignal('');
     const description = useSignal('');
     const board = useSignal({name: 'Choose a board'});
-    const taggedTopics = useSignal('');
 
+    const [taggedTopics,setTaggedTopics] = useState([]);
     const [allowComments, setAllowComments] = useState(true);
+    const [showProducts, setShowProducts] = useState(true);
     const [isBoardFocus, setIsBoardFocus] = useState(false);
+    const [isTopicFocus, setIsTopicFocus] = useState(false);
     const [isMoreOption, setIsMoreOption] = useState(false);
 
     const [imageLink, setImageLink] = useState('');
@@ -25,21 +45,39 @@ function PinForm() {
     const [invalidImg, setInvalidImg] = useState(false); 
 
     const boardDiv = useRef(null);
+    const topicDiv = useRef(null);
 
     const handleSubmit = () => {
-        console.log("title :", title.value, "imageLink : ", imageLink, "description :" , description.value, "board : ", board.value, "tagged Topics" , taggedTopics.value, "allowCom : ", allowComments );
+        console.log("title :", title.value, "imageLink : ", imageLink, "description :" , description.value, "board : ", board.value, "tagged Topics" , taggedTopics, "allowCom : ", allowComments );
     }
-    const onBoardSelect = (selected) => {
-        console.log('slslfks', selected);
+    const onBoardSelect = (id) => {
+        let selected = boards.value.find(ele => ele.id === id); //find board by id
         board.value = selected;
         setIsBoardFocus(false);
         boardDiv.current.blur();
     } 
+    const onTopicSelect = (id) => {
+
+        let index = topics.value.findIndex(ele => ele.id === id); //find index
+        //let selected;
+        if(index !== -1){
+            let selected = {...topics.value[index]};
+            selected.color = `rgb(${Math.random()*100}, ${Math.random()*100}, ${Math.random()*100})`;
+            taggedTopics.push(selected);
+            topics.value = topics.value.filter((ele) => ele.id !== id); //remove from topics
+            setIsTopicFocus(false);
+            topicDiv.current.blur();
+        }
+    }
+
     const handleMoreOption = () => {
         setIsMoreOption(!isMoreOption);
     }
-    const handleSwitch = () =>{
+    const handleAllowComments = () =>{
         setAllowComments(!allowComments);
+    }
+    const handleShowProducts = () =>{
+        setShowProducts(!allowComments);
     }
     const handleInputChange = (e) => {
         const inputValue = e.target.value;
@@ -66,23 +104,35 @@ function PinForm() {
         };
     };
     const handleKeyPress = (e) => {
+        e.preventDefault();
         if(e.key === 'Enter'){
             handleImageValidation();
         }
     }
-
     const handleEdit = () => {
         setImageLink('');
         setIsValidImage(false);
+    }
+
+    const removeTopic = (e) => {
+        e.preventDefault();
+        let index = taggedTopics.findIndex(ele => ele.id === e.target.id);
+        if(index !== -1) {
+            let removed = {...taggedTopics[index]};
+            topics.value.push(removed); 
+            setTaggedTopics(taggedTopics.filter((ele) => ele.id !== e.target.id));
+            console.log("remove", topics.value, removed, e.target.id, taggedTopics);
+        }
     }
     
     return (
         <>
             <div className="heading">
                 Create Pin
-                <div className="publishBtn" onClick={handleSubmit}>
+                <button className="publishBtn" onClick={handleSubmit} disabled={!isValidImage} 
+                    style={{backgroundColor : `${isValidImage ? 'red' : 'grey'}`, cursor : `${isValidImage ? 'pointer' : 'no-drop'}`}}>
                     Publish
-                </div>
+                </button>
             </div>
             <div className="pinFormContainer">
                 { !isValidImage &&
@@ -122,25 +172,41 @@ function PinForm() {
                         <label className='pinLabel'>Board:</label>
                         <div className="boardInput" ref={boardDiv} 
                             tabIndex={0} //for make div focusable
-                            onFocus={() => {setIsBoardFocus(true); console.log('onfoucs');}}
+                            onFocus={() => setIsBoardFocus(true)}
                             onBlur={() => setIsBoardFocus(false)}
                         >
-                            {board.value?.name}
+                            {board.value?.image && 
+                                <img src={board.value?.image} alt={board.value?.name} />
+                            }
+                            <p>{board.value?.name}  </p>
                             <FaChevronDown className='downArrow' size={'1.2rem'}/>
-                            {isBoardFocus &&
+                        {isBoardFocus &&
+                            <div className="optionsBox">
+                                <SelectBoardBox onSelect={onBoardSelect} boxName="board" array={boards.value}/>
+                            </div>
+                        }
+                        </div>
+                        <label className='pinLabel'>Tagged Topics:</label>
+                        <div className="boardInput" ref={topicDiv} 
+                            tabIndex={0} //for make div focusable
+                            onFocus={() => setIsTopicFocus(true)}
+                            onBlur={() => setIsTopicFocus(false)}
+                        >   Select a Tag
+                            {isTopicFocus &&
                                 <div className="optionsBox">
-                                    <SelectBoardBox onSelect={onBoardSelect}/>
+                                    <SelectBoardBox onSelect={onTopicSelect} boxName="topic" array={topics.value}/>
                                 </div>
                             }
                         </div>
-                        <label className='pinLabel'>Tagged Topics:</label>
-                        <input className='pinInput'
-                            placeholder='Search for a Tag'
-                            type="text"
-                            value={taggedTopics}
-                            onChange={(e) => taggedTopics.value = e.target.value}
-                            required
-                        />
+                        <div className='showTaggedTopics'>
+                            {taggedTopics.map((topic) => (
+                                <div key={topic.id} className="topic" style={{backgroundColor : topic?.color}}>
+                                    {topic?.name}
+                                    <IoCloseSharp className='closeIcon' size={'1.4rem'} id={topic.id} onClick={removeTopic}/>
+                                </div>
+                            ))
+                            }
+                        </div>
 
                         <div className="moreOptions" >
                             <h4 onClick={handleMoreOption}> 
@@ -151,16 +217,36 @@ function PinForm() {
                             </h4>
                             { isMoreOption &&
                             <>
-                                <Switch
-                                checked={allowComments.value} 
-                                onClick={handleSwitch}
-                                color="default"
-                                size='6rem'
-                                style={{
-                                    color: 'black'
-                                }}
-                                />
-                                <span> Allow people to comment </span>
+                                <div>
+                                    <Switch
+                                    checked={allowComments} 
+                                    onClick={handleAllowComments}
+                                    color="default"
+                                    size='6rem'
+                                    style={{
+                                        color: 'black'
+                                    }}
+                                    />
+                                    <span> Allow people to comment </span>
+                                </div>
+                                <div>
+                                    <Switch
+                                    checked={showProducts} 
+                                    onClick={handleShowProducts}
+                                    color="default"
+                                    size='6rem'
+                                    style={{
+                                        color: 'black'
+                                    }}
+                                    />
+                                    <span> Show Similar products </span>
+                                </div>
+                                <p>
+                                People can shop for products similar to what's shown in this Pin using visual search
+                                </p>
+                                <p>
+                                Shopping recommendations aren't available for Idea ads and Pins with tagged products or paid partnership labels
+                                </p>
                             </>
                             }  
                             
--- src/components/SelectBoardBox/SelectBoardBox.jsx
+++ src/components/SelectBoardBox/SelectBoardBox.jsx
@@ -5,37 +5,56 @@ import { FaCirclePlus } from "react-icons/fa6";
 
 import './SelectBoardBox.css';
 
-function SelectBoardBox({onSelect}) {
-    const boards = useSignal([
-        { id: 'board1', name: 'Board 1', isLocked: true, image: 'https://i.pinimg.com/75x75/8b/0b/e3/8b0be3749ca05848ce6b7d3fe5d3983f.jpg' },
-        { id: 'board2', name: 'Board 2', isLocked: false, image: 'https://i.pinimg.com/75x75/92/29/64/922964c806076e8975d494af90dcdffe.jpg' },
-        { id: 'board3', name: 'Board 3', isLocked: true, image: 'https://i.pinimg.com/75x75/8b/0b/e3/8b0be3749ca05848ce6b7d3fe5d3983f.jpg' },
-        { id: 'board4', name: 'Board 4', isLocked: false, image: 'https://i.pinimg.com/75x75/92/29/64/922964c806076e8975d494af90dcdffe.jpg' },
-    ]);
-
-    const handleClick = (e) => {
+function SelectBoardBox({onSelect, boxName, array}) {
+    console.log(array);
+    const handleClickBoard = (e) => {
         //console.log(e.target.id);
-        let selected = boards.value.find(ele => ele.id === e.target.id);
-        //console.log(selected);
-        onSelect(selected);
+        onSelect(e.target.id);
     }
+
+    const handleClickTopic = (e) => {
+        //console.log(e.target.id);
+        onSelect(e.target.id);
+    }
+
     return (
-        <div className='boardBox'>
-            <h5>All Boards</h5>
-            {boards.value.map((board) => (
-                <div key={board.id} className="boardOption" id={board.id} onClick={handleClick}>
-                    <img src={board.image} alt={board.name} />
-                    <p className='boardName'> {board.name}</p>
-                    { board.isLocked && 
-                        <IoMdLock className='lockIcon' size={'1.4rem'}/>
-                    }
+        <>
+        { boxName ==="board" ?
+            <div className='boardBox'>
+                <h5>All Boards</h5>
+                {array.map((board) => (
+                    <>{board?.id !== undefined && 
+                        <div key={board?.id} className="boardOption" id={board?.id} onClick={handleClickBoard}>
+                            <img src={board?.image} alt={board?.name} />
+                            <p className='boardName'> {board?.name}</p>
+                            { board.isLocked && 
+                                <IoMdLock className='lockIcon' size={'1.4rem'}/>
+                            }
+                        </div>
+                        }
+                    </>
+                ))}
+                <div className="createBoard">
+                    <FaCirclePlus className='createIcon' size={'2rem'} />
+                    <p>Create Board </p>
                 </div>
-            ))}
-            <div className="createBoard">
-                <FaCirclePlus className='createIcon' size={'2rem'} />
-                <p>Create Board </p>
             </div>
-        </div>
+            :
+            <div className='boardBox'>
+                <h5>All Topics</h5>
+                {array.map((topic) => (
+                    <div key={topic?.id} className="boardOption" id={topic?.id} onClick={handleClickTopic}>
+                        <p className='topicName'> {topic?.name}</p>
+                    </div>
+                ))}
+                {array.length === 0 &&
+                    <p className='noTopic'> All Selected</p>
+                }
+            </div>
+
+        }
+
+        </>
     )
 }
 
